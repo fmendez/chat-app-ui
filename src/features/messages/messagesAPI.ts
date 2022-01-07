785806bf83f444761662dfaedf49f663606b22ac
Fernando Mendez <fmendez.paradis@gmail.com>
2022-01-07 11:42:14 -0400

    Remove unused code

--- src/features/messages/messagesAPI.ts
+++ src/features/messages/messagesAPI.ts
@@ -1,18 +1,4 @@
-import { MessageType } from "../../types";
-
 export const fetchMessages = async (): Promise<any> => {
   const res = await fetch("http://localhost:3090/messages")
   return await res.json()
-}
-
-export const sendMessage = async (message: string): Promise<any> => {
-  const res = await fetch("http://localhost:3090/messages", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({messages: {content: message}})
-  });
-
- return await res.json()
 }
